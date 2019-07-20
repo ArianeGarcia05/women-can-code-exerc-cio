bb43a2b8d0f7c70b8a812429731061bb1300f1b3
ariane <arianegarciaa05@gmail.com>
2019-07-20 10:53:39 -0300

    Adicionado projeto inicial

---
+++ src/test/java/com/projetofinal/aula6/Aula6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projetofinal.aula6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aula6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
